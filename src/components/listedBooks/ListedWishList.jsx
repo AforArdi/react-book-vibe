76bb596b44a5cee2410dd8769dd0eca7a1232115
AforArdi <binsbro36@gmail.com>
2026-04-06 18:24:52 +0600

    default message div added with conditional rendering for both wishlist and readlist component

--- src/components/listedBooks/ListedWishList.jsx
+++ src/components/listedBooks/ListedWishList.jsx
@@ -1,9 +1,20 @@
 import { useContext } from "react";
 import { BookContext } from "../../contexts/BookProvider";
 import BookCard from "../ui/BookCard";
+import { FaBookmark } from "react-icons/fa";
 
 const ListedWishList = () => {
     const {wishList} = useContext(BookContext);
+
+    if(wishList.length === 0){
+        return (
+            <div className="h-[50vh] flex flex-col items-center justify-center bg-gray-100 rounded-lg">
+                <FaBookmark size={40}></FaBookmark>
+                <h2 className="font-bold text-3xl">No Books in Wish List</h2>
+            </div>
+        )
+    }
+
     return ( 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             {
--- src/pages/books/Books.jsx
+++ src/pages/books/Books.jsx
@@ -2,23 +2,12 @@ import { useContext } from "react";
 import { BookContext } from "../../contexts/BookProvider";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import BookCard from "../../components/ui/BookCard";
-import { FaBookmark } from "react-icons/fa";
 import ListedReadList from "../../components/listedBooks/ListedReadList";
 import ListedWishList from "../../components/listedBooks/ListedWishList";
 
 const Books = () => {
     const {wishList, readBooks} = useContext(BookContext);
     // console.log(wishList, readBooks);
-
-    if(readBooks.length === 0){
-        return (
-            <div>
-                <FaBookmark></FaBookmark>
-                <h2 className="font-bold text-3xl">No Books in Read List</h2>
-            </div>
-        )
-    }
 
     return ( 
         <div className="container mx-auto">
