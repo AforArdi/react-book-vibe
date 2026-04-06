0cb0de71c88cd319537cf5a223fef74eb953cf69
AforArdi <binsbro36@gmail.com>
2026-04-06 20:08:39 +0600

    navbar sticky

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     </li>
     </>
     return ( 
-        <nav className="bg-base-100 shadow-sm">
+        <nav className="bg-base-100 shadow-sm sticky top-0 z-50">
             <div className="navbar container mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
